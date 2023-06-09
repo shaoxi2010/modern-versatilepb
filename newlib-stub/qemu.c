916e49a080219f8aac7a1f20e7901ebfa54c354d
shaoxi2010 <shaoxi2010@qq.com>
2023-06-09 10:45:13 +0800

    修改初始化

--- newlib-stub/qemu.c
+++ newlib-stub/qemu.c
@@ -3,13 +3,17 @@
 
 __attribute__((noreturn)) void _exit(int status)
 {
-	register int reg0 asm("r0") __attribute__((unused));
-	register int reg1 asm("r1") __attribute__((unused));
+	int reg0 = 0x18;	// angel_SWIreason_ReportException
+	int reg1 = 0x20026; // ADP_Stopped_ApplicationExit
 
-	reg0 = 0x18;	// angel_SWIreason_ReportException
-	reg1 = 0x20026; // ADP_Stopped_ApplicationExit
 
-	asm("svc 0x00123456"); // make semihosting call
+	asm("mov r0, %0 \n\t"   // angel_SWIreason_ReportException
+		"mov r1, %1\n\t" // ADP_Stopped_ApplicationExit
+		"svc 0x00123456\n\t"   // make semihosting call
+		:
+        :"r"(reg0), "r"(reg1)
+		: "r0", "r1");
 
-    while (1);
+	while (1)
+		;
 }
